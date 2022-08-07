93e8499ee32c9c594a7a491902cfd46ec57faa9d
claudioluciano <claudio_luciano@live.com>
2022-08-07 20:44:47 +0000

    feat: :lipstick: Add windicss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+// eslint-disable-next-line
+// @ts-nocheck
 import { defineConfig } from 'vite'
+import { resolve as resolvePath } from 'path'
 import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolvePath(__dirname, './src')
+    }
+  },
+  plugins: [vue(), WindiCSS()]
 })
