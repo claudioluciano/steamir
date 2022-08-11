68630050b5566b950d23e75ba49b4f334130af3d
claudioluciano <claudio_luciano@live.com>
2022-08-11 02:41:37 +0000

    feat: Config unplug-icons

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import { resolve as resolvePath } from 'path'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +18,8 @@ export default defineConfig({
       '@': resolvePath(__dirname, './src')
     }
   },
-  plugins: [vue(), WindiCSS()]
+  plugins: [vue(), WindiCSS(), Icons({
+    compiler: 'vue3',
+    autoInstall: true
+  })]
 })
